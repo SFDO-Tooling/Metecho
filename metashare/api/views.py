356fa9a5278926c21f26e7eb30bf982f1c8302c1
Kit La Touche <kit@transneptune.net>
2019-06-27 12:34:44 -0600

    Fix linting

--- metashare/api/views.py
+++ metashare/api/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics, status, viewsets
-from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
