b8e5590d2c9e06160519fa5f6bdac324a4a2edbb
erica <mitchell.erica31@gmail.com>
2019-07-03 14:32:58 -0400

    cleanup, removing initial avatar

--- src/js/components/products/detail.tsx
+++ src/js/components/products/detail.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import DocumentTitle from 'react-document-title';
 import i18n from 'i18next';
 import { connect } from 'react-redux';
 import { AppState } from '@/store';
 import { Link } from 'react-router-dom';
-import Button from '@salesforce/design-system-react/components/button';
+
 import BreadCrumb from '@salesforce/design-system-react/components/breadcrumb';
 import PageHeader from '@salesforce/design-system-react/components/page-header';
+
 import routes from '@/utils/routes';
+import { Product } from '@/store/products/reducer';
 
 import {
   selectProduct,
@@ -16,58 +18,47 @@ import {
 } from '@/store/products/selectors';
 
 interface Props {
-  product: {};
+  product: Product;
   productSlug: string;
 }
-const ProductDetail = props => {
+const ProductDetail = ({ product, productSlug }: Props) => {
+  console.log();
   return (
-    <DocumentTitle
-      title={`${i18n.t(props.productSlug)} | ${i18n.t('MetaShare')}`}
-    >
+    <DocumentTitle title={`${i18n.t(productSlug)} | ${i18n.t('MetaShare')}`}>
       <>
         <PageHeader
           className="page-header slds-p-around_x-large"
           iconAssistiveText={{ icon: 'User' }}
           iconCategory="standard"
-          icon={
-            <span className="slds-avatar">
-              <abbr
-                className="slds-avatar__initials slds-icon-standard-account"
-                title="company name"
-              >
-                Ac
-              </abbr>
-            </span>
-          }
-          title={props.product.name}
+          title={product.name}
           label="Erica Mitchell"
-          variant="user"
         />
         <div className="slds-p-around_large">
           <div className="slds-grid slds-gutters">
             <div className="slds-col slds-size_2-of-3 ">
+              {/* @todo make into reusable component?  */}
               <BreadCrumb
                 assistiveText={{ label: 'Two item breadcrumb' }}
                 trail={[
                   <Link to={routes.home()}>Home</Link>,
                   <Link
                     className="slds-text-link_reset"
-                    to={routes.product_detail(props.productSlug)}
+                    to={routes.product_detail(productSlug)}
                   >
-                    {props.product.name}
+                    {product.name}
                   </Link>,
                 ]}
               />
             </div>
             <div className="slds-col slds-size_1-of-3">
               <h2 className="slds-m-top_large slds-m-bottom_small slds-text-heading_small">
-                [{props.product.name}]
+                [{product.name}]
               </h2>
               <p
                 className="markdown slds-p-bottom_small"
-                dangerouslySetInnerHTML={{ __html: props.product.description }}
+                dangerouslySetInnerHTML={{ __html: product.description }}
               />
-              <Link to={props.product.repo_url}>Link to Github Repo</Link>
+              <a href={product.repo_url}>Link to Github Repo</a>
             </div>
           </div>
         </div>
