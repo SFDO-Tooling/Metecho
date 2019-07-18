960fbc9c731931095ae6bccc3b0b895870b5e542
Kit La Touche <kit@transneptune.net>
2019-07-18 11:23:30 -0600

    📦 Render Markdown at the serializer layer

--- metashare/api/fields.py
+++ metashare/api/fields.py
@@ -1,15 +1,45 @@
 from rest_framework.fields import CharField
+import bleach
+from markdown import markdown
+
+
+# These two constants are duplicated from sfdo-template-helpers' MarkdownField.
+# Not the best thing, but the clearest way to render markdown out of a serializer.
+ALLOWED_TAGS = [
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "b",
+    "i",
+    "strong",
+    "em",
+    "tt",
+    "p",
+    "br",
+    "span",
+    "div",
+    "blockquote",
+    "code",
+    "hr",
+    "ul",
+    "ol",
+    "li",
+    "dd",
+    "dt",
+    "img",
+    "a",
+]
+
+ALLOWED_ATTRS = {"img": ["src", "alt", "title"], "a": ["href", "alt", "title"]}
+
+
+def render_clean_markdown(raw_md):
+    return bleach.clean(markdown(raw_md), tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRS)
 
 
 class MarkdownField(CharField):
     def to_representation(self, value):
-        # This presumes that you are bound to a serializer and have an
-        # instance on that serializer, neither of which I like, but it
-        # does avoid duplicating the logic in the model MarkdownField.
-        # print(">>>>", self.parent.instance)
-        parent = (
-            self.parent.instance[0]
-            if isinstance(self.parent.instance, list)
-            else self.parent.instance
-        )
-        return getattr(parent, self.field_name + "_markdown")
+        return render_clean_markdown(value)
