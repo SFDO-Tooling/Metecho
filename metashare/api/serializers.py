cb745328ffa4d348bb08a887a112984ede68b315
Kit La Touche <kit@transneptune.net>
2019-07-31 11:31:01 -0600

    Add better error message for duplicated Task name within project

--- metashare/api/serializers.py
+++ metashare/api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
@@ -71,7 +72,7 @@ class ProjectSerializer(serializers.ModelSerializer):
             UniqueTogetherValidator(
                 queryset=Project.objects.all(),
                 fields=("name", "product"),
-                message="A project with this name already exists.",
+                message=_("A project with this name already exists."),
             ),
         )
 
@@ -99,4 +100,11 @@ class TaskSerializer(serializers.ModelSerializer):
             "assignee",
             "slug",
             "old_slugs",
+        )
+        validators = (
+            UniqueTogetherValidator(
+                queryset=Task.objects.all(),
+                fields=("name", "project"),
+                message=_("A task with this name already exists."),
+            ),
         )
