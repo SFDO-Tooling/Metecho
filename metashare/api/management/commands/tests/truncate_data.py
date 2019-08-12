1f1f27b3a4ae615d1d8690c09b7fc6a5d6bd0880
Kit La Touche <kit@transneptune.net>
2019-08-12 12:41:45 -0600

    🛢 Rename Product to Repository everywhere on the backend
    This will break the frontend, as the URLs for these resources have
    changed now.

--- metashare/api/management/commands/tests/truncate_data.py
+++ metashare/api/management/commands/tests/truncate_data.py
@@ -1,19 +1,19 @@
 import pytest
 from django.core.management import call_command
 
-from ....models import Product, ProductSlug
+from ....models import Repository, RepositorySlug
 
 
 @pytest.mark.django_db
-def test_truncate_data(product_factory):
-    product_factory(repo_url="https://example.com/test-repo.git")
-    product_factory(repo_url="https://example.com/test-repo2.git")
-    product_factory(repo_url="https://example.com/test-repo3.git")
+def test_truncate_data(repository_factory):
+    repository_factory(repo_url="https://example.com/test-repo.git")
+    repository_factory(repo_url="https://example.com/test-repo2.git")
+    repository_factory(repo_url="https://example.com/test-repo3.git")
 
-    assert ProductSlug.objects.count() == 3
-    assert Product.objects.count() == 3
+    assert RepositorySlug.objects.count() == 3
+    assert Repository.objects.count() == 3
 
     call_command("truncate_data")
 
-    assert ProductSlug.objects.count() == 0
-    assert Product.objects.count() == 0
+    assert RepositorySlug.objects.count() == 0
+    assert Repository.objects.count() == 0
