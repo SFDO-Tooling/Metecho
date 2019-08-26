6bee70ae6b9738a4b5f16654f9152e83993dfe26
erica <mitchell.erica31@gmail.com>
2019-08-26 16:12:33 -0400

    add state for panels and checkboxes

--- src/js/components/tasks/capture.tsx
+++ src/js/components/tasks/capture.tsx
@@ -7,27 +7,54 @@ import Modal from '@salesforce/design-system-react/components/modal';
 import i18n from 'i18next';
 import React, { useState } from 'react';
 
+import { useForm } from '@/components/utils';
+import { OBJECT_TYPES } from '@/utils/constants';
 const mockList = {
-  'Apex Classes': [{ id: '1', name: 'Class 1' }],
-  'Custom Objects': [{ id: '1', name: 'Custom objects' }],
-  'Class others': [{ id: '1', name: 'Class others' }],
-  FooBars: [{ id: '1', name: 'FooBars' }],
-  Feefitfum: [{ id: '1', name: 'Feefitfum' }],
-  Whatchamacallit: [{ id: '1', name: 'Whatchamacallit' }],
-  'Loopy ': [{ id: '1', name: 'Loopy' }],
+  ApexClasses: [{ id: '1', name: 'Class 1' }],
+  CustomObjects: [{ id: '2', name: 'Custom objects' }],
+  ClassOthers: [{ id: '3', name: 'Class others' }],
+  FooBars: [{ id: '4', name: 'Foo Bars' }],
+  Feefitfum: [{ id: '5', name: 'Fee fitfum' }],
+  'Whatcha macallit': [{ id: '6', name: 'Whatchamacallit' }],
+  'Loopy ': [{ id: '7', name: 'Loopy Looo' }],
 };
 
-const CaptureModal = ({ isOpen }) => {
-  const handleCaptureChanges = () => console.log('handle action');
-  const [expandedPanels, setExpandedPanels] = useState(new Set());
+interface Props {
+  isOpen: boolean;
+}
+const CaptureModal = ({ isOpen }: Props) => {
+  const [expandedPanels, setExpandedPanels] = useState({});
+  const [checkedItems, setCheckedItems] = useState({});
+  const [checkboxes, setCheckBoxes] = useState(mockList);
+  const [selectAllChanges, setSelectAllChanges] = useState(false);
 
   const handlePanelToggle = (id: string) => {
-    if (expandedPanels.has(id)) {
-      setExpandedPanels(expandedPanels.delete(id));
-    } else {
-      setExpandedPanels(expandedPanels.add(id));
+    setExpandedPanels({ ...checkedItems, [id]: !expandedPanels[id] });
+  };
+
+  const onSuccess = (action: AnyAction) => {
+    console.log('success');
+  };
+  const handleChange = (event: React.BaseSyntheticEvent, value: string) => {
+    setCheckedItems({ ...checkedItems, [value]: event.target.checked });
+    if (value === 'select-all') {
+      setCheckedItems({});
+      setSelectAllChanges(!selectAllChanges);
     }
   };
+
+  const {
+    inputs,
+    errors,
+    handleInputChange,
+    handleSubmit,
+    resetForm,
+  } = useForm({
+    fields: { changes: [], message: '' },
+    objectType: OBJECT_TYPES.TASK,
+    onSuccess,
+  });
+
   return (
     <Modal
       heading={i18n.t('Select the changes you wish to capture')}
@@ -35,26 +62,24 @@ const CaptureModal = ({ isOpen }) => {
       size="large"
       align="top"
     >
-      <form className="slds-p-around_large" onSubmit={handleCaptureChanges}>
+      <form className="slds-p-around_large" onSubmit={handleSubmit}>
         <Checkbox
           assistiveText={{
             label: `${i18n.t('Select All')}`,
           }}
-          id="checkbox-example"
+          id="select-all"
           labels={{
             label: `${i18n.t('Select All')}`,
           }}
-          onChange={e => {
-            console.log('onChange ', e.target);
-          }}
+          onChange={(e: any) => handleChange(e, 'select-all')}
         />
-        {Object.keys(mockList).map((item, idx) => {
+        {Object.keys(checkboxes).map((item, idx) => {
           const panelId = `${item}-${idx}`;
 
           return (
             <Accordion key={idx} className="" id="base-example-accordion">
               <AccordionPanel
-                expanded={expandedPanels.has(panelId)}
+                expanded={expandedPanels[panelId]}
                 id={panelId}
                 onTogglePanel={() => handlePanelToggle(panelId)}
                 summary={
@@ -63,38 +88,56 @@ const CaptureModal = ({ isOpen }) => {
                     assistiveText={{
                       label: item,
                     }}
-                    id="checkbox-example"
+                    id={panelId}
                     label
                     labels={{
                       label: item,
                     }}
-                    onChange={e => {
-                      console.log(e.target.value);
-                    }}
+                    onChange={(e: any) => handleChange(e, item)}
                     name="changes"
+                    checked={
+                      selectAllChanges ? selectAllChanges : checkedItems[item]
+                    }
                   />
                 }
               >
-                {mockList[item].map((list: any) => (
-                  <Checkbox
-                    key={`${list}-${idx}`}
-                    assistiveText={{
-                      label: list.name,
-                    }}
-                    id="checkbox-example"
-                    label
-                    labels={{
-                      label: list.name,
-                    }}
-                    onChange={e => {
-                      console.log('onChange ', e.target);
-                    }}
-                  />
-                ))}
+                {checkboxes[item].map((list: any) => {
+                  const checked =
+                    selectAllChanges ||
+                    checkedItems[item] ||
+                    checkedItems[list.name];
+                  return (
+                    <Checkbox
+                      key={`${list}-${idx}`}
+                      assistiveText={{
+                        label: list.name,
+                      }}
+                      id={`${list.name}-${idx}`}
+                      label
+                      labels={{
+                        label: list.name,
+                      }}
+                      onChange={e => handleChange(e, list.name)}
+                      checked={checked}
+                    />
+                  );
+                })}
               </AccordionPanel>
             </Accordion>
           );
         })}
+        <Input
+          id="commit-message"
+          label={i18n.t('Commit Message')}
+          className="slds-form-element_stacked slds-p-left_none"
+          name="name"
+          value={inputs.message}
+          required
+          aria-required
+          maxLength="50"
+          errorText={errors.message}
+          onChange={handleInputChange}
+        />
       </form>
     </Modal>
   );
