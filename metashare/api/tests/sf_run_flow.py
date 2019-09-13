c33edd136e31b958e4241d36a8cc052f8d6126c6
Kit La Touche <kit@transneptune.net>
2019-09-12 22:21:33 -0600

    Tidy up

--- metashare/api/tests/sf_run_flow.py
+++ metashare/api/tests/sf_run_flow.py
@@ -199,7 +199,6 @@ class TestDeployOrgSettings:
 
 def test_create_org_and_run_flow():
     with ExitStack() as stack:
-        stack.enter_context(patch(f"{PATCH_ROOT}.SocialApp"))
         stack.enter_context(patch(f"{PATCH_ROOT}.os"))
         stack.enter_context(patch(f"{PATCH_ROOT}.BaseCumulusCI"))
         stack.enter_context(patch(f"{PATCH_ROOT}.get_devhub_api"))
