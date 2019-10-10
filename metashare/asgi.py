476f9b52c06c1d8dc0fb8d91a3cf715777227418
Kit La Touche <kit@transneptune.net>
2019-10-10 11:32:57 -0600

    Add Sentry ASGI integration

--- metashare/asgi.py
+++ metashare/asgi.py
@@ -6,14 +6,22 @@ defined in the ASGI_APPLICATION setting.
 import os
 
 import django
+import sentry_sdk
 from channels.routing import get_default_application
 from newrelic import agent
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
 agent.initialize()
 agent.wrap_web_transaction("django.core.handlers.base", "BaseHandler.get_response")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+SENTRY_DSN = os.environ.get("SENTRY_DSN")
+if SENTRY_DSN:  # pragma: nocover
+    sentry_sdk.init(dsn=SENTRY_DSN)
 
 django.setup()
 
-application = get_default_application()
+if SENTRY_DSN:  # pragma: nocover
+    application = SentryAsgiMiddleware(get_default_application())
+else:
+    application = get_default_application()
--- metashare/consumers.py
+++ metashare/consumers.py
@@ -49,25 +49,10 @@ class PushNotificationConsumer(AsyncJsonWebsocketConsumer):
         if "content" in event:
             await self.send_json(event["content"])
             return
-        # if "serializer" in event and "instance" in event and "inner_type" in event:
-        #     instance = self.get_instance(**event["instance"])
-        #     serializer = self.get_serializer(event["serializer"])
-        #     payload = {
-        #         "payload": serializer(
-        #             instance=instance, context=user_context(self.scope["user"])
-        #         ).data,
-        #         "type": event["inner_type"],
-        #     }
-        #     await self.send_json(payload)
-        #     return
 
     def get_instance(self, *, model, id, **kwargs):
         Model = apps.get_model("api", model)
         return Model.objects.get(pk=id)
-
-    # def get_serializer(self, serializer_path):
-    #     mod, serializer = serializer_path.rsplit(".", 1)
-    #     return getattr(import_module(mod), serializer)
 
     async def receive_json(self, content, **kwargs):
         # Just used to sub/unsub to notification channels.
