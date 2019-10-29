f29ef1c401e66fe28b59c135e62f1c52de4dd27e
Jonny Gerig Meyer <jonny@oddbird.net>
2019-10-29 14:11:10 -0400

    Remove duplicate sentry.init

--- metashare/asgi.py
+++ metashare/asgi.py
@@ -6,7 +6,6 @@ defined in the ASGI_APPLICATION setting.
 import os
 
 import django
-import sentry_sdk
 from channels.routing import get_default_application
 from newrelic import agent
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
@@ -15,12 +14,10 @@ agent.initialize()
 agent.wrap_web_transaction("django.core.handlers.base", "BaseHandler.get_response")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-SENTRY_DSN = os.environ.get("SENTRY_DSN")
-if SENTRY_DSN:  # pragma: nocover
-    sentry_sdk.init(dsn=SENTRY_DSN)
 
 django.setup()
 
+SENTRY_DSN = os.environ.get("SENTRY_DSN")
 if SENTRY_DSN:  # pragma: nocover
     application = SentryAsgiMiddleware(get_default_application())
 else:
