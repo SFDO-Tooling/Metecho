fbf236b3a68814db47a6b722757644f9f9387d3c
Jonny Gerig Meyer <jonny@oddbird.net>
2019-11-01 10:46:46 -0400

    🧶 Upgrade front-end deps.

--- src/js/components/404.tsx
+++ src/js/components/404.tsx
@@ -1,4 +1,3 @@
-import Illustration from '@salesforce/design-system-react/components/illustration';
 import i18n from 'i18next';
 import React, { ReactNode } from 'react';
 import DocumentTitle from 'react-document-title';
@@ -9,13 +8,20 @@ import routes from '@/utils/routes';
 import svgPath from '#/desert.svg';
 
 export const EmptyIllustration = ({ message }: { message: ReactNode }) => (
-  <Illustration
-    heading="¯\_(ツ)_/¯"
-    messageBody={message}
-    name="Desert"
-    path={`${svgPath}#desert`}
-    size="large"
-  />
+  <div className="slds-illustration slds-illustration_large">
+    <svg
+      className="slds-illustration__svg"
+      aria-hidden="true"
+      name="desert"
+      style={{ height: '400px' }}
+    >
+      <use xlinkHref={`${svgPath}#desert`} />
+    </svg>
+    <h3 className="slds-illustration__header slds-text-heading_medium">
+      ¯\_(ツ)_/¯
+    </h3>
+    <p className="slds-text-body_regular">{message}</p>
+  </div>
 );
 
 const FourOhFour = ({ message }: { message?: ReactNode }) => (
