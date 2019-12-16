8fbe6b721d648b3de2347e338d4b2dad1a68f6be
Kit La Touche <kit@transneptune.net>
2019-12-16 10:42:46 -0700

    📦 Update DRF and change tests accordingly

--- metashare/api/validators.py
+++ metashare/api/validators.py
@@ -13,21 +13,23 @@ class CaseInsensitiveUniqueTogetherValidator(UniqueTogetherValidator):
             return "name__iexact"
         return field_name
 
-    def filter_queryset(self, attrs, queryset):
+    def filter_queryset(self, attrs, queryset, serializer):
         """
         Filter the queryset to all instances matching the given attributes.
         """
+        # field names => field sources
+        sources = [serializer.fields[field_name].source for field_name in self.fields]
+
         # If this is an update, then any unprovided field should
-        # have its value set based on the existing instance attribute.
-        if self.instance is not None:
-            for field_name in self.fields:
-                if field_name not in attrs:
-                    attrs[field_name] = getattr(self.instance, field_name)
+        # have it's value set based on the existing instance attribute.
+        if serializer.instance is not None:
+            for source in sources:
+                if source not in attrs:
+                    attrs[source] = getattr(serializer.instance, source)
 
         # Determine the filter keyword arguments and filter the queryset.
         filter_kwargs = {
-            self.process_field_name(field_name): attrs[field_name]
-            for field_name in self.fields
+            self.process_field_name(source): attrs[source] for source in sources
         }
         return qs_filter(queryset, **filter_kwargs)
 
