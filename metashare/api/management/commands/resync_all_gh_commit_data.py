9b04884a00bc435aa0f37e8fca49c3d7289a10c6
Kit La Touche <kit@transneptune.net>
2019-12-17 12:55:24 -0700

    Add management command to resync all GH data

---
+++ metashare/api/management/commands/resync_all_gh_commit_data.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+
+from ...jobs import refresh_commits
+from ...models import Task
+
+
+class Command(BaseCommand):
+    help = "Remove and resync all stored commits from GitHub."
+
+    def handle(self, *args, **options):
+        for task in Task.objects.exclude(branch_name__isnull=True):
+            refresh_commits(
+                repository=task.project.repository, branch_name=task.branch_name
+            )
