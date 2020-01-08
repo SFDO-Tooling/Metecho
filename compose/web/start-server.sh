7f6bb54d869df401559c7d2d168838feac555027
Kit La Touche <kit@transneptune.net>
2020-01-08 13:12:36 -0700

    Suppress sync warnings in Django restart
    This is a stop-gap measure to allow automatically restarting the server
    in development. In production, we don't have this issue, so that's good.
    The error appeared to be arising not directly from any code we had
    written, and I had no luck isolating it to one component (Sentry, New
    Relic, the websocket consumer, and possibly other components were all
    candidates), so I opted to suppress the warning and allow restart in
    dev, instead.

--- compose/web/start-server.sh
+++ compose/web/start-server.sh
@@ -4,7 +4,7 @@ if [ -z ${PORT+x} ];
 then
     # PORT unset, we presume this is local dev:
     python manage.py migrate
-    yarn serve
+    DJANGO_ALLOW_ASYNC_UNSAFE=true yarn serve
 else
     # PORT set, we presume this is Heroku:
     yarn django:serve:prod
