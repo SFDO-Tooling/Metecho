5e950f4f8080380ff51f65fd0253e78cb6e5a93a
Kit La Touche <kit@transneptune.net>
2020-01-08 15:01:07 -0700

    Start to think through the frontend here

--- src/js/components/projects/repositoryGitHubUsers.tsx
+++ src/js/components/projects/repositoryGitHubUsers.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
-const UserCard = ({ user }) => <li>{user.login}</li>;
+
+//TODO:
+// - Make these useful cards.
+// - Insert these in the correct place in the document.
+// - The list of Repo users should probably be a modal, and a separate
+//   component, compared to the list of Project users, yes?
+
+const UserCard = ({ user, addToProject }) => <li onclick={() => addToProject(user)}>{user.login}</li>;
 
 export default ({ users }) => (
+  const addToProject = (user) => {
+    // What pattern are we using here? Should this go through the store,
+    // triggering an API call incidentally, or just send an API call directly?
+  }
+
   <ul>
     {users.map((user) => (
-      <UserCard user={user} />
+      <UserCard user={user} addToProject={addToProject} />
     ))}
   </ul>
 );
