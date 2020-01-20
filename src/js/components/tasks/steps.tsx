067fbf5ba1f2ee07aea0254e387d91186cddbfa3
Erica Mitchell <mitchell.erica31@gmail.com>
2020-01-20 18:33:25 -0500

    wip states for active, queued steps

--- src/js/components/tasks/steps.tsx
+++ src/js/components/tasks/steps.tsx
@@ -1,7 +1,7 @@
 import RadioGroup from '@salesforce/design-system-react/components/radio-group';
 import Radio from '@salesforce/design-system-react/components/radio-group/radio';
 import classNames from 'classnames';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Task } from '@/store/tasks/reducer';
 import { TASK_STATUSES } from '@/utils/constants';
@@ -11,6 +11,30 @@ interface TaskStatusPathProps {
 }
 
 const TaskStatusSteps = ({ task }: TaskStatusPathProps) => {
+  const [completedSteps, setcompletedSteps] = useState([]);
+  const [nextSteps, setNextSteps] = useState([]);
+
+  useEffect(() => {
+    const inProgressDone = task.has_unmerged_commits ? [0, 1] : [0, 1, 2, 3]; // @todo this one might be wrong
+    const inProgressNext = task.has_unmerged_commits ? [3] : [4];
+    let done, next;
+    switch (task.status) {
+      case TASK_STATUSES.PLANNED:
+        done = [];
+        next = [0, 1];
+        break;
+      case TASK_STATUSES.IN_PROGRESS:
+        done = inProgressDone;
+        next = inProgressNext;
+        break;
+      case TASK_STATUSES.COMPLETED:
+        done = [0, 1, 2, 3, 4, 5]; // or maybe all the indexes of steps ?
+        next = [];
+        break;
+    }
+    setcompletedSteps(done);
+    setNextSteps(next);
+  }, [task]);
   const steps = [
     'Assign developer',
     'Assign reviewer',
@@ -19,48 +43,30 @@ const TaskStatusSteps = ({ task }: TaskStatusPathProps) => {
     'Submit task changes for review',
     'Create a QA Org for development',
   ];
-  const isNext = true;
-  const isDone = false;
-  // set class/styles based on if item is included in 'next' or 'done'
-  const checkedStatus = classNames({
-    'is-next': isNext,
-    'is-done': isDone,
-  });
-
-  const inProgressDone = task.has_unmerged_commits ? [0, 1] : [0, 1, 2];
-  const inProgressNext = task.has_unmerged_commits ? [3] : [4];
-  let done, next;
-  switch (task.status) {
-    case TASK_STATUSES.PLANNED:
-      done = null;
-      next = [0, 1];
-      break;
-    case TASK_STATUSES.IN_PROGRESS:
-      done = inProgressDone;
-      next = inProgressNext;
-      break;
-    case TASK_STATUSES.COMPLETED:
-      done = steps.length; // or maybe, all the indexes of steps ?
-      next = null;
-      break;
-  }
-  // @todo: assign the right classes based on if index is is done or next,
   // @todo render different steps for login as dev or reviewer
 
+  console.log(completedSteps, nextSteps);
   return (
     <RadioGroup labels={{ label: 'Next Steps' }} disabled={false} name="steps">
-      {steps.map((item, index) => (
-        <Radio
-          key={index}
-          id={item}
-          labels={{ label: item }}
-          value={item}
-          checked={true}
-          variant="base"
-          status={task.status}
-          className={checkedStatus}
-        />
-      ))}
+      {steps.map((item, index) => {
+        const stepIsNext = _.includes(nextSteps, index);
+        const stepIsComplete = _.includes(completedSteps, index);
+        const checkedStatus = classNames({
+          'is-next': stepIsNext,
+          'is-done': stepIsComplete,
+        });
+        return (
+          <Radio
+            key={index}
+            id={item}
+            labels={{ label: item }}
+            value={item}
+            variant="base"
+            status={task.status}
+            className={checkedStatus}
+          />
+        );
+      })}
     </RadioGroup>
   );
 };
