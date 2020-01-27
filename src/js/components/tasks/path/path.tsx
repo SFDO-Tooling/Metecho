98409dd5aa31753b635803f83fd980cb666cde72
Stacy Kvernmo <stacy@oddbird.net>
2020-01-27 14:52:37 -0600

    wrap path in small region div

--- src/js/components/tasks/path/path.tsx
+++ src/js/components/tasks/path/path.tsx
@@ -14,35 +14,37 @@ const Path = ({ current, children, status, previousSteps }: PathProps) => {
   const lastIndex = filteredChildren.length - 1;
 
   return (
-    <div className="ms-task-status-path slds-path">
-      <div className="slds-grid slds-path__track">
-        <div className="slds-grid slds-path">
-          <ul
-            className="ms-task-status-path__list slds-path__nav slds-m-left_none"
-            role="listbox"
-            aria-orientation="horizontal"
-          >
-            {Children.map(
-              filteredChildren,
-              (child: CElement<HTMLElement, any>, index) => {
-                if (!child) {
-                  return null;
-                }
-                const stepNumber = 0 + index; // not sure of what this is for right now...
-                const childProps = {
-                  stepNumber: `${stepNumber + 1}`,
-                  stepIndex: stepNumber,
-                  status,
-                  previousSteps,
-                  activeStep: false,
-                  lastIndex,
-                };
-                childProps.activeStep = stepNumber === current;
+    <div className="slds-region_small slds-m-bottom_x-large">
+      <div className="ms-task-status-path slds-path">
+        <div className="slds-grid slds-path__track">
+          <div className="slds-grid slds-path">
+            <ul
+              className="ms-task-status-path__list slds-path__nav slds-m-left_none"
+              role="listbox"
+              aria-orientation="horizontal"
+            >
+              {Children.map(
+                filteredChildren,
+                (child: CElement<HTMLElement, any>, index) => {
+                  if (!child) {
+                    return null;
+                  }
+                  const stepNumber = 0 + index; // not sure of what this is for right now...
+                  const childProps = {
+                    stepNumber: `${stepNumber + 1}`,
+                    stepIndex: stepNumber,
+                    status,
+                    previousSteps,
+                    activeStep: false,
+                    lastIndex,
+                  };
+                  childProps.activeStep = stepNumber === current;
 
-                return cloneElement(child, childProps);
-              },
-            )}
-          </ul>
+                  return cloneElement(child, childProps);
+                },
+              )}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
