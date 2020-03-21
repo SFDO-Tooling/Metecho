855f376bdc13dcfe805f1439d3c7aea0214aa3ee
Jonny Gerig Meyer <jonny@oddbird.net>
2020-03-21 17:02:16 -0400

    lint

--- test/js/components/utils/editModal.test.jsx
+++ test/js/components/utils/editModal.test.jsx
@@ -1,4 +1,4 @@
-import { fireEvent, waitFor } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import React from 'react';
 
 import EditModal from '@/components/utils/editModal';
