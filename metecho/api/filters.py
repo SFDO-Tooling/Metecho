a7ab01eb4cde036c8ed1a359267a4ba63baf903c
Kit La Touche <kit@transneptune.net>
2020-04-08 14:16:40 -0600

    Omit inactive slugs

--- metecho/api/filters.py
+++ metecho/api/filters.py
@@ -3,8 +3,12 @@ from django_filters import rest_framework as filters
 from .models import Project, Repository, ScratchOrg, Task
 
 
+def slug_is_active(queryset, name, value):
+    return queryset.filter(**{f"{name}__slug": value, f"{name}__is_active": True})
+
+
 class RepositoryFilter(filters.FilterSet):
-    slug = filters.CharFilter(field_name="slugs__slug")
+    slug = filters.CharFilter(field_name="slugs", method=slug_is_active)
 
     class Meta:
         model = Repository
@@ -12,7 +16,7 @@ class RepositoryFilter(filters.FilterSet):
 
 
 class ProjectFilter(filters.FilterSet):
-    slug = filters.CharFilter(field_name="slugs__slug")
+    slug = filters.CharFilter(field_name="slugs", method=slug_is_active)
 
     class Meta:
         model = Project
@@ -20,7 +24,7 @@ class ProjectFilter(filters.FilterSet):
 
 
 class TaskFilter(filters.FilterSet):
-    slug = filters.CharFilter(field_name="slugs__slug")
+    slug = filters.CharFilter(field_name="slugs", method=slug_is_active)
 
     class Meta:
         model = Task
