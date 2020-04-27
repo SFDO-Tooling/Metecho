88a17d48f0155efec6351fb93e9c7d3e36dc6ab4
Erica Mitchell <mitchell.erica31@gmail.com>
2020-04-27 14:23:08 -0400

    replace card with picker

--- src/js/components/repositories/listItem.tsx
+++ src/js/components/repositories/listItem.tsx
@@ -1,29 +1,40 @@
-import Card from '@salesforce/design-system-react/components/card';
+import Icon from '@salesforce/design-system-react/components/icon';
+import VisualPicker from '@salesforce/design-system-react/components/visual-picker';
+import VisualPickerLink from '@salesforce/design-system-react/components/visual-picker/link';
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import { Repository } from '@/store/repositories/reducer';
 import routes from '@/utils/routes';
 
 const RepositoryListItem = ({ repository }: { repository: Repository }) => (
-  <Link
-    to={routes.repository_detail(repository.slug)}
+  <VisualPicker
+    size="large"
+    id={repository.slug}
     className="slds-text-link_reset
       slds-p-around_small
       slds-size_1-of-1
       slds-medium-size_1-of-2
       slds-large-size_1-of-3"
   >
-    <Card heading={repository.name} bodyClassName="slds-card__body_inner">
-      {repository.description_rendered ? (
-        <div
-          className="truncate-children slds-p-top_x-small"
-          // This description is pre-cleaned by the API
-          dangerouslySetInnerHTML={{ __html: repository.description_rendered }}
-        />
-      ) : null}
-    </Card>
-  </Link>
+    <VisualPickerLink
+      href={routes.repository_detail(repository.slug)}
+      icon={<Icon category="custom" name="custom6" size="large" />}
+      title={repository.name}
+      className="bar"
+      links
+      description={
+        repository.description_rendered ? (
+          <div
+            className="truncate-children slds-p-top_x-small"
+            // This description is pre-cleaned by the API
+            dangerouslySetInnerHTML={{
+              __html: repository.description_rendered,
+            }}
+          />
+        ) : null
+      }
+    ></VisualPickerLink>
+  </VisualPicker>
 );
 
 export default RepositoryListItem;
