62a4fb1bb73d48f589931c08a31156ee30d39df6
Jonny Gerig Meyer <jonny@oddbird.net>
2020-05-07 17:35:28 -0400

    Add type for comboboxFilter.

--- src/js/@types/design-system-react.d.ts
+++ src/js/@types/design-system-react.d.ts
@@ -16,3 +16,31 @@ declare module '@salesforce/design-system-react/components/settings' {
   };
   export default settings;
 }
+
+declare module '@salesforce/design-system-react/components/combobox/filter' {
+  type Option = {
+    id: string;
+    icon?: JSX.Element;
+    label?: string;
+    subTitle?: string;
+    type?: string;
+    disabled?: boolean;
+    tooltipContent?: JSX.Element;
+  };
+  type Selection = {
+    id: string;
+    icon?: JSX.Element;
+    label?: string;
+    subTitle?: string;
+    type?: string;
+  };
+
+  const filter: (opts: {
+    inputValue: string;
+    limit: number;
+    options: Option[];
+    selection: Selection[];
+  }) => Option[];
+
+  export default filter;
+}
