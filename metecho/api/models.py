f42745dbe03a7239e5d6912759b83fd12f5d77a7
Jonny Gerig Meyer <jonny@oddbird.net>
2020-05-14 16:02:27 -0400

    Enforce Task.(name, project) uniqueness in business logic

--- metecho/api/models.py
+++ metecho/api/models.py
@@ -716,7 +716,10 @@ class Task(
 
     class Meta:
         ordering = ("-created_at", "name")
-        unique_together = (("name", "project"),)
+        # We enforce this in business logic, not in the database, as we
+        # need to limit this constraint only to active Tasks, and
+        # make the name column case-insensitive:
+        # unique_together = (("name", "project"),)
 
 
 class ScratchOrg(
