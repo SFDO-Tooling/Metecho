2dd17e634636102ff8ef14f25c30ed7f69749865
Kit La Touche <kit@transneptune.net>
2020-05-21 13:55:45 -0600

    Add merge migration

--- metecho/api/migrations/0073_merge_20200520_1931.py
+++ metecho/api/migrations/0073_merge_20200520_1931.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0072_githubrepository_repo_image_url'),
-        ('api', '0072_merge_20200423_1445'),
+        ("api", "0072_githubrepository_repo_image_url"),
+        ("api", "0072_merge_20200423_1445"),
     ]
 
-    operations = [
-    ]
+    operations = []
