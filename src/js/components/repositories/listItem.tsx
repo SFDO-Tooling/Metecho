d8a4c1d123d7842c81d874d0f06277437ea8b2b8
Erica Mitchell <mitchell.erica31@gmail.com>
2020-05-21 20:48:08 -0400

    adds avatar for repo img

--- src/js/components/repositories/listItem.tsx
+++ src/js/components/repositories/listItem.tsx
@@ -1,3 +1,4 @@
+import Avatar from '@salesforce/design-system-react/components/avatar';
 import VisualPicker from '@salesforce/design-system-react/components/visual-picker';
 import VisualPickerLink from '@salesforce/design-system-react/components/visual-picker/link';
 import i18n from 'i18next';
@@ -7,43 +8,43 @@ import { Repository } from '@/store/repositories/reducer';
 import routes from '@/utils/routes';
 
 const RepositoryListItem = ({ repository }: { repository: Repository }) => (
-  <VisualPicker
-    size="large"
-    id={repository.slug}
-    className="slds-text-link_reset
+  <div data-form="repo-select">
+    <VisualPicker
+      size="large"
+      id={repository.slug}
+      className="slds-text-link_reset
       slds-p-around_small
       slds-size_1-of-1
       slds-medium-size_1-of-2
       slds-large-size_1-of-3"
-  >
-    <VisualPickerLink
-      href={routes.repository_detail(repository.slug)}
-      icon={
-        <img
-          src={
-            repository.repo_image_url
-              ? repository.repo_image_url
-              : 'https://via.placeholder.com/75'
-          }
-          alt={`${i18n.t('image for')} ${repository.name}}`}
-        />
-      }
-      title={repository.name}
-      className="bar"
-      links
-      description={
-        repository.description_rendered ? (
-          <div
-            className="truncate-children slds-p-top_x-small"
-            // This description is pre-cleaned by the API
-            dangerouslySetInnerHTML={{
-              __html: repository.description_rendered,
-            }}
+    >
+      <VisualPickerLink
+        href={routes.repository_detail(repository.slug)}
+        icon={
+          <Avatar
+            variant="entity"
+            label={repository.name}
+            size="large"
+            imgSrc={repository.repo_image_url}
+            imgAlt={`${i18n.t('Image for')} ${repository.name}`}
           />
-        ) : null
-      }
-    ></VisualPickerLink>
-  </VisualPicker>
+        }
+        title={repository.name}
+        links
+        description={
+          repository.description_rendered ? (
+            <span
+              className="truncate-children slds-p-top_x-small"
+              // This description is pre-cleaned by the API
+              dangerouslySetInnerHTML={{
+                __html: repository.description_rendered,
+              }}
+            />
+          ) : null
+        }
+      ></VisualPickerLink>
+    </VisualPicker>
+  </div>
 );
 
 export default RepositoryListItem;
