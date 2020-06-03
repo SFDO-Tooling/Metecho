4be7f8e5e90a7868aacddb5d6d4a62a30ca2b654
Erica Mitchell <mitchell.erica31@gmail.com>
2020-06-03 13:22:01 -0400

    fix project step stati

--- src/js/components/projects/steps.tsx
+++ src/js/components/projects/steps.tsx
@@ -4,46 +4,50 @@ import React from 'react';
 import Steps from '@/components/steps';
 import { Project } from '@/store/projects/reducer';
 import { Task } from '@/store/tasks/reducer';
+import { PROJECT_STATUSES } from '@/utils/constants';
 
 interface ProjectStatusStepsProps {
   tasks: Task[] | [];
   project: Project;
 }
 const ProjectStatusSteps = ({ tasks, project }: ProjectStatusStepsProps) => {
+  const hasTasks = Boolean(tasks.length);
+  const hasDev = tasks.some((task) => task.assigned_dev);
   const steps = [
     {
       label: `${i18n.t('Create a task')}`,
       visible: true,
-      active: !tasks?.length,
-      complete: Boolean(tasks.length), // complete if there are 1+ tasks
-      assignee: null, //
+      active: !hasTasks,
+      complete: hasTasks,
+      assignee: null,
     },
     {
       label: `${i18n.t('Assign a developer to a task')}`,
       visible: true,
-      active: Boolean(tasks.length),
-      complete: Boolean(tasks.length && project.github_users.length), // complete if there are tasks collaborators
-      assignee: null, // anyone can add collaborators
+      active: hasTasks && !hasDev,
+      complete: project.has_unmerged_commits || (hasTasks && hasDev),
+      assignee: null,
     },
     {
       label: `${i18n.t('Complete a task')}`,
       visible: true,
-      active: false, // if there is a task with an unmerged commit
-      complete: false, // not sure, if the no unmerged commits and
+      active: hasTasks && hasDev, //  active if tasks exist && any task has a developer
+      complete: project.has_unmerged_commits, // project.has_unmerged_commits
       assignee: null,
     },
     {
       label: `${i18n.t('Submit project for review')}`,
       visible: true,
-      active: false,
-      complete: false,
+      active: project.has_unmerged_commits,
+      complete:
+        project.pr_is_open || project.status === PROJECT_STATUSES.MERGED, //  project.pr_is_open || project.status === MERGED
       assignee: null,
     },
     {
       label: `${i18n.t('Merge pull request on Github')}`,
       visible: true,
-      active: false,
-      complete: false,
+      active: project.pr_is_open,
+      complete: project.status === PROJECT_STATUSES.MERGED,
       assignee: null,
     },
   ];
