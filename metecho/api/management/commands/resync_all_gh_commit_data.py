4fc360b9f08c0970e439b96c3093914daa3ed961
Kit La Touche <kit@transneptune.net>
2020-06-09 15:50:17 -0600

    De Morganize

--- metecho/api/management/commands/resync_all_gh_commit_data.py
+++ metecho/api/management/commands/resync_all_gh_commit_data.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.db.models import Q
 
 from ...jobs import refresh_commits
 from ...models import Task
@@ -8,7 +9,7 @@ class Command(BaseCommand):
     help = "Remove and resync all stored commits from GitHub."
 
     def handle(self, *args, **options):
-        for task in Task.objects.exclude(branch_name="", origin_sha=""):
+        for task in Task.objects.exclude(Q(branch_name="") | Q(origin_sha="")):
             refresh_commits(
                 repository=task.project.repository,
                 branch_name=task.branch_name,
