6b84e72886c62c90dd63dedf7137dec1f308f42e
Stacy Kvernmo <stacy@oddbird.net>
2020-06-10 13:32:52 -0500

    increase padding between button and cards after removing inner wrapper.

--- src/js/components/user/githubUser.tsx
+++ src/js/components/user/githubUser.tsx
@@ -76,7 +76,7 @@ export const UserCards = ({
     className="slds-grid
       slds-wrap
       slds-grid_pull-padded-xx-small
-      slds-m-top_medium"
+      slds-m-top_large"
   >
     {users.map((user) => {
       const doRemoveUser = () => removeUser(user);
