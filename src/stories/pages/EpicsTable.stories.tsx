444b1d161783fc5a7d969b3efffd146cd35da762
Jonny Gerig Meyer <jonny@oddbird.net>
2021-01-22 11:39:16 -0500

    Add fixtures.

--- src/stories/pages/EpicsTable.stories.tsx
+++ src/stories/pages/EpicsTable.stories.tsx
@@ -2,7 +2,13 @@ import { Story } from '@storybook/react/types-6-0';
 import React, { ComponentProps } from 'react';
 
 import EpicsTableComponent from '~js/components/epics/table';
-import { EPIC_STATUSES } from '~js/utils/constants';
+
+import {
+  sampleEpic1,
+  sampleEpic2,
+  sampleEpic3,
+  sampleEpic4,
+} from '../fixtures';
 
 export default {
   title: 'Pages/Epics/Table/Component',
@@ -16,118 +22,6 @@ const Template: Story<ComponentProps<typeof EpicsTableComponent>> = (args) => (
 
 export const EpicsTable = Template.bind({});
 EpicsTable.args = {
-  epics: [
-    {
-      id: 'e1',
-      project: 'my-project',
-      name: 'My Epic',
-      slug: 'my-epic',
-      old_slugs: [],
-      description: 'Epic Description',
-      description_rendered: '<p>Epic Description</p>',
-      branch_name: 'feature/my-epic',
-      branch_url: 'https://github.com/test/test-repo/tree/feature/my-epic',
-      branch_diff_url:
-        'https://github.com/test/test-repo/compare/main...feature/my-epic',
-      pr_url: null,
-      pr_is_open: false,
-      pr_is_merged: false,
-      has_unmerged_commits: false,
-      currently_creating_pr: false,
-      currently_fetching_org_config_names: false,
-      github_users: [],
-      status: EPIC_STATUSES.PLANNED,
-      available_task_org_config_names: [],
-    },
-    {
-      id: 'e2',
-      project: 'my-project',
-      name: 'Mid-Year Project Saturn',
-      slug: 'midyear-project-saturn',
-      old_slugs: [],
-      description:
-        'Stabilize existing structures and provide clarity to team members.',
-      description_rendered:
-        '<p>Stabilize existing structures and provide clarity to team members.</p>',
-      branch_name: 'feature/midyear-project-saturn',
-      branch_url:
-        'https://github.com/test/test-repo/tree/feature/midyear-project-saturn',
-      branch_diff_url:
-        'https://github.com/test/test-repo/compare/main...feature/midyear-project-saturn',
-      pr_url: 'https://github.com/test/test-repo/pull/1234',
-      pr_is_open: true,
-      pr_is_merged: false,
-      has_unmerged_commits: true,
-      currently_creating_pr: false,
-      currently_fetching_org_config_names: false,
-      github_users: [
-        {
-          id: '123456',
-          login: 'TestGitHubUser',
-          avatar_url: 'https://randomuser.me/api/portraits/men/1.jpg',
-        },
-        {
-          id: '234567',
-          login: 'OtherUser',
-          avatar_url: 'https://randomuser.me/api/portraits/women/12.jpg',
-        },
-        {
-          id: '345678',
-          login: 'ThirdUser',
-          avatar_url: 'https://randomuser.me/api/portraits/men/123.jpg',
-        },
-      ],
-      status: EPIC_STATUSES.IN_PROGRESS,
-      available_task_org_config_names: [],
-    },
-    {
-      id: 'e3',
-      project: 'my-project',
-      name: 'Regular Database Backups',
-      slug: 'database-backups',
-      old_slugs: [],
-      description: 'Fulfilling the requirements to access specific tech specs.',
-      description_rendered:
-        '<p>Fulfilling the requirements to access specific tech specs.</p>',
-      branch_name: 'feature/database-backups',
-      branch_url:
-        'https://github.com/test/test-repo/tree/feature/database-backups',
-      branch_diff_url:
-        'https://github.com/test/test-repo/compare/main...feature/database-backups',
-      pr_url: null,
-      pr_is_open: false,
-      pr_is_merged: false,
-      has_unmerged_commits: false,
-      currently_creating_pr: false,
-      currently_fetching_org_config_names: false,
-      github_users: [],
-      status: EPIC_STATUSES.PLANNED,
-      available_task_org_config_names: [],
-    },
-    {
-      id: 'e4',
-      project: 'my-project',
-      name: 'Data Controls',
-      slug: 'data-controls',
-      old_slugs: [],
-      description: 'Links the controller field to the static items',
-      description_rendered:
-        '<p>Links the controller field to the static items</p>',
-      branch_name: 'feature/data-controls',
-      branch_url:
-        'https://github.com/test/test-repo/tree/feature/data-controls',
-      branch_diff_url:
-        'https://github.com/test/test-repo/compare/main...feature/data-controls',
-      pr_url: null,
-      pr_is_open: false,
-      pr_is_merged: false,
-      has_unmerged_commits: false,
-      currently_creating_pr: false,
-      currently_fetching_org_config_names: false,
-      github_users: [],
-      status: EPIC_STATUSES.MERGED,
-      available_task_org_config_names: [],
-    },
-  ],
+  epics: [sampleEpic1, sampleEpic2, sampleEpic3, sampleEpic4],
   projectSlug: 'my-project',
 };
--- src/stories/pages/TasksTable.stories.tsx
+++ src/stories/pages/TasksTable.stories.tsx
@@ -5,6 +5,13 @@ import React, { ComponentProps } from 'react';
 import TasksTableComponent from '~js/components/tasks/table';
 
 import { withRedux } from '../decorators';
+import {
+  sampleEpic1,
+  sampleGitHubUser1,
+  sampleGitHubUser2,
+  sampleGitHubUser3,
+  sampleTask1,
+} from '../fixtures';
 
 export default {
   title: 'Pages/Tasks/Table/Component',
@@ -26,72 +33,9 @@ const Template: Story<ComponentProps<typeof TasksTableComponent>> = (args) => (
 export const TasksTable = Template.bind({});
 TasksTable.args = {
   projectSlug: 'my-project',
-  epicSlug: 'data-controls',
-  tasks: [
-    {
-      id: 'M13MnQO',
-      name: 'Data Mapping',
-      description: 'This is a description',
-      description_rendered: '<p>This is <em>safely</em> rendered Markdown.</p>',
-      epic: '3Lw7OwK',
-      assigned_dev: {
-        id: '123456',
-        login: 'TestGitHubUser',
-        avatar_url: 'https://randomuser.me/api/portraits/men/1.jpg',
-      },
-      assigned_qa: null,
-      slug: 'test-task',
-      old_slugs: [],
-      has_unmerged_commits: true,
-      currently_creating_pr: false,
-      branch_name: 'feature/test-epic__test-task',
-      branch_url:
-        'https://github.com/SFDO-Tooling/test-project/tree/feature/test-epic__test-task',
-      branch_diff_url:
-        'https://github.com/SFDO-Tooling/test-project/compare/feature/test-epic...feature/test-epic__test-task',
-      commits: [
-        {
-          id: '617a512',
-          timestamp: '2019-02-01T19:47:49Z',
-          author: {
-            name: 'Full Name',
-            username: 'TestGitHubUser',
-            email: 'user@example.com',
-            avatar_url: 'https://randomuser.me/api/portraits/men/1.jpg',
-          },
-          message: 'Some commit message',
-          url: 'https://github.com/SFDO-Tooling/commit/617a512',
-        },
-      ],
-      origin_sha: '723b342',
-      pr_url: 'https://github.com/SFDO-Tooling/test-project/pull/1357',
-      pr_is_open: true,
-      status: 'Planned',
-      currently_submitting_review: false,
-      review_submitted_at: '2019-03-01T19:47:49Z',
-      review_valid: true,
-      review_status: 'Approved',
-      review_sha: '617a512',
-      org_config_name: 'dev',
-    },
-  ],
-  epicUsers: [
-    {
-      id: '123456',
-      login: 'TestGitHubUser',
-      avatar_url: 'https://randomuser.me/api/portraits/men/1.jpg',
-    },
-    {
-      id: '234567',
-      login: 'OtherUser',
-      avatar_url: 'https://randomuser.me/api/portraits/women/1.jpg',
-    },
-    {
-      id: '345678',
-      login: 'ThirdUser',
-      avatar_url: 'https://randomuser.me/api/portraits/men/2.jpg',
-    },
-  ],
+  epicSlug: sampleEpic1.slug,
+  tasks: [sampleTask1],
+  epicUsers: [sampleGitHubUser1, sampleGitHubUser2, sampleGitHubUser3],
   openAssignEpicUsersModal: action('openAssignEpicUsersModal'),
   assignUserAction: action('assignUserAction'),
 };
