59aaa44ece840301f1c43a3c0955cde0715956de
Erica Mitchell <mitchell.erica31@gmail.com>
2021-01-26 08:54:50 -0500

    epics --> epic

---
+++ src/stories/epics/EpicTable.stories.tsx
@@ -0,0 +1,27 @@
+import { Story } from '@storybook/react/types-6-0';
+import React, { ComponentProps } from 'react';
+
+import EpicTableComponent from '~js/components/epics/table';
+
+import {
+  sampleEpic1,
+  sampleEpic2,
+  sampleEpic3,
+  sampleEpic4,
+} from '../fixtures';
+
+export default {
+  title: 'Epics/Table/Component',
+  component: EpicTableComponent,
+  description: 'something here',
+};
+
+const Template: Story<ComponentProps<typeof EpicTableComponent>> = (args) => (
+  <EpicTableComponent {...args} />
+);
+
+export const EpicTable = Template.bind({});
+EpicTable.args = {
+  epics: [sampleEpic1, sampleEpic2, sampleEpic3, sampleEpic4],
+  projectSlug: 'my-project',
+};
