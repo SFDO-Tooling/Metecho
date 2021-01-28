901440e4e06e3f1fe3438be6e9e2d5885a75398b
Jonny Gerig Meyer <jonny@oddbird.net>
2021-01-28 08:49:39 -0500

    comment

--- src/stories/decorators.tsx
+++ src/stories/decorators.tsx
@@ -5,6 +5,7 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
 const storyMiddleware = () => (next: (act: any) => any) => (act: any) => {
+  // Log Redux action to Storybook "Actions" panel
   action('dispatch')(act);
   return next(act);
 };
