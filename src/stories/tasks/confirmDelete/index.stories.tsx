29cbb158b58453d4077479a2ffb06410d197cd41
dvdherron <dvdherron@gmail.com>
2021-05-10 12:05:58 +0000

    add confirmDelete component in storybook

--- src/stories/tasks/confirmDelete/index.stories.tsx
+++ src/stories/tasks/confirmDelete/index.stories.tsx
@@ -1,20 +1,18 @@
-import React, { ComponentProps } from 'react';
-import { Story } from '@storybook/react/types-6-0';
 import { action } from '@storybook/addon-actions';
+import { Story } from '@storybook/react/types-6-0';
+import React, { ComponentProps } from 'react';
 
+import ConfirmDeleteModal from '~js/components/tasks/confirmDeleteModal';
 import { Org } from '~js/store/orgs/reducer';
-import ConfirmDeleteModal from '~js/components/tasks/confirmDeleteModal'
-
 
 export default {
   title: 'Tasks/ConfirmDelete/Example',
   component: ConfirmDeleteModal,
 };
 
-const Template: Story<ComponentProps<typeof ConfirmDeleteModal>> = (
-  args,
-  ) => <ConfirmDeleteModal {...args} />
-
+const Template: Story<ComponentProps<typeof ConfirmDeleteModal>> = (args) => (
+  <ConfirmDeleteModal {...args} />
+);
 
 export const ConfirmDelete = Template.bind({});
 
@@ -27,6 +25,6 @@ ConfirmDelete.args = {
 ConfirmDelete.argTypes = {
   org: { control: { disable: true } },
   isOpen: { control: { disable: true } },
-}
+};
 
 ConfirmDelete.storyName = 'Example';
