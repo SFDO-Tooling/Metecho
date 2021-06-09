4a7b031b2560b905dcaa4afb7f5d93cfb14a7fbd
dvdherron <dvdherron@gmail.com>
2021-06-09 02:10:21 +0000

    edit confirmDelete component and fix commitList

--- src/stories/commits/CommitList.stories.tsx
+++ src/stories/commits/CommitList.stories.tsx
@@ -2,12 +2,14 @@ import { Story } from '@storybook/react/types-6-0';
 import React, { ComponentProps } from 'react';
 
 import CommitListComponent from '~js/components/commits/list';
+import { withRedux } from '../decorators';
 
 import { sampleCommit1, sampleCommit2 } from '../fixtures';
 
 export default {
   title: 'Commits/List/Example',
   component: CommitListComponent,
+  decorators: [withRedux()],
 };
 
 const Template: Story<ComponentProps<typeof CommitListComponent>> = (args) => (
--- src/stories/tasks/confirmDelete/index.stories.tsx
+++ src/stories/tasks/confirmDelete/index.stories.tsx
@@ -2,12 +2,19 @@ import { action } from '@storybook/addon-actions';
 import { Story } from '@storybook/react/types-6-0';
 import React, { ComponentProps } from 'react';
 
+ import {  ConfirmOrgTracker,CONFIRM_ORG_TRACKER, ORG_TYPES } from '~js/utils/constants';
+
 import ConfirmDeleteModal from '~js/components/tasks/confirmDeleteModal';
 
 export default {
   title: 'Tasks/ConfirmDelete/Example',
   component: ConfirmDeleteModal,
 };
+
+const actionTypes: { [key: string]: ConfirmOrgTracker } = {
+  'Delete': 'delete',
+  'Refresh': 'refresh',
+}
 
 const Template: Story<ComponentProps<typeof ConfirmDeleteModal>> = (args) => (
   <ConfirmDeleteModal {...args} />
@@ -18,12 +25,20 @@ export const ConfirmDelete = Template.bind({});
 ConfirmDelete.args = {
   org: null,
   isOpen: true,
+  actionType: 'delete',
   handleClose: action('handleClose'),
   handleCancel: action('handleCancel'),
 };
 ConfirmDelete.argTypes = {
   org: { control: { disable: true } },
   isOpen: { control: { disable: true } },
+  actiontype: {
+    options: Object.keys(actionTypes),
+    control: {
+      type: 'select',
+    }
+  }
+
 };
 
 ConfirmDelete.storyName = 'Example';
