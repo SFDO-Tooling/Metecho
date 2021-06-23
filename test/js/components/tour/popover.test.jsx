2415ee05bb2de765ade2ddc464358a219edb0d76
dvdherron <dvdherron@gmail.com>
2021-06-23 17:16:13 +0000

    edit tests

--- test/js/components/tour/popover.test.jsx
+++ test/js/components/tour/popover.test.jsx
@@ -1,10 +1,11 @@
+import { fireEvent, waitFor } from '@testing-library/react';
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 
 import TourPopover from '~js/components/tour/popover';
 import { updateTour } from '~js/store/user/actions';
 
-import { renderWithRedux } from '../../utils';
+import { renderWithRedux, storeWithThunk } from '../../utils';
 
 jest.mock('~js/store/user/actions');
 updateTour.mockReturnValue(() => Promise.resolve({ type: 'TEST' }));
@@ -20,6 +21,7 @@ describe('<TourPopover />', () => {
         <TourPopover />
       </MemoryRouter>,
       state,
+      storeWithThunk,
     );
 
   let ENABLE_WALKTHROUGHS;
@@ -49,5 +51,18 @@ describe('<TourPopover />', () => {
     });
 
     expect(queryByText('Learn More', { exact: false })).toBeNull();
+  });
+
+  test('calls UpdateTour action', () => {
+    const { getByRole } = setup({
+      user: {
+        self_guided_tour_enabled: true,
+        self_guided_tour_state: [],
+      },
+      projects: {},
+    });
+    fireEvent.click(getByRole('button', { name: 'Learn More' }));
+
+    expect(updateTour).toHaveBeenCalledTimes(1);
   });
 });
