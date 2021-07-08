572227321397314200508f0094402c7d8f1a38df
dvdherron <dvdherron@gmail.com>
2021-07-08 12:53:48 +0000

    render confirmRemove component

---
+++ src/stories/tasks/confirmRemove/index.stories.tsx
@@ -0,0 +1,32 @@
+import { action } from '@storybook/addon-actions';
+import { Story } from '@storybook/react/types-6-0';
+import React, { ComponentProps } from 'react';
+
+import ConfirmRemoveUserModal from '~js/components/tasks/confirmRemoveUserModal';
+import { withRedux } from '../../decorators';
+
+import { ORG_TYPES } from '~js/utils/constants';
+
+export default {
+  title: 'Tasks/ConfirmRemove/Example',
+  component: ConfirmRemoveUserModal,
+  decorators: [withRedux()],
+};
+
+const Template: Story<ComponentProps<typeof ConfirmRemoveUserModal>> = (
+  args,
+) => <ConfirmRemoveUserModal {...args} />;
+
+export const ConfirmRemove = Template.bind({});
+ConfirmRemove.args = {
+  isOpen: true,
+  waitingToRemoveUser: {
+    type: ORG_TYPES.PLAYGROUND,
+    assignee: null,
+    shouldAlertAssignee: false,
+  },
+  handleClose: action('handleClose'),
+  handleCancel: action('handleCancel'),
+};
+
+ConfirmRemove.storyName = 'Example';
