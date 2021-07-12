8da81d5bb45ff922b334eff6828152a5a12b333b
dvdherron <dvdherron@gmail.com>
2021-07-12 14:18:26 +0000

    add confirmRemove documentation

--- src/stories/tasks/confirmRemove/index.stories.tsx
+++ src/stories/tasks/confirmRemove/index.stories.tsx
@@ -6,6 +6,7 @@ import ConfirmRemoveUserModal from '~js/components/tasks/confirmRemoveUserModal'
 import { withRedux } from '../../decorators';
 
 import { ORG_TYPES } from '~js/utils/constants';
+import { sampleUser1 } from '../../fixtures';
 
 export default {
   title: 'Tasks/ConfirmRemove/Example',
@@ -21,12 +22,13 @@ export const ConfirmRemove = Template.bind({});
 ConfirmRemove.args = {
   isOpen: true,
   waitingToRemoveUser: {
-    type: ORG_TYPES.PLAYGROUND,
+    type: ORG_TYPES.DEV,
     assignee: null,
     shouldAlertAssignee: false,
   },
   handleClose: action('handleClose'),
   handleCancel: action('handleCancel'),
+  handleAssignUser: action('handleAssignUser')
 };
 
 ConfirmRemove.storyName = 'Example';
