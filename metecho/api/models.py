2b4bb02415faaaea0b8f3b3301ccadc5f5e512ed
dvdherron <dvdherron@gmail.com>
2021-09-01 12:53:40 +0000

    some backend copy

--- metecho/api/models.py
+++ metecho/api/models.py
@@ -1019,11 +1019,11 @@ class ScratchOrg(
     def clean(self):
         if len([x for x in [self.project, self.epic, self.task] if x is not None]) != 1:
             raise ValidationError(
-                _("A ScratchOrg must belong to either a project, an epic, or a task.")
+                _("A Scratch Org must belong to either a Project, an Epic, or a Task.")
             )
         if self.org_type != SCRATCH_ORG_TYPES.Playground and not self.task:
             raise ValidationError(
-                {"org_type": _("Dev and Test orgs must belong to a task.")}
+                {"org_type": _("Dev and Test Orgs must belong to a Task.")}
             )
         return super().clean()
 
