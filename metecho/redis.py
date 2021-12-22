3bb4fd6ffc2aa090f98c6fcae44ebe0510c5b613
Jonny Gerig Meyer <jonny@oddbird.net>
2021-12-22 16:31:05 -0500

    Update metecho/redis.py
    Co-authored-by: Peter Conrad <peter.conrad@salesforce.com>

--- metecho/redis.py
+++ metecho/redis.py
@@ -2,7 +2,7 @@ from redis.connection import ConnectionPool, SSLConnection
 
 
 class SSLConnectionPool(ConnectionPool):
-    """Connection pool that disable SSL checks by default"""
+    """Connection pool that disables SSL checks by default"""
 
     def __init__(
         self, connection_class=SSLConnection, max_connections=None, **connection_kwargs
