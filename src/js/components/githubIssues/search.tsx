13d47ac14d61c8c9f0287c64627425c4efdf3040
Olu Niyi-Awosusi <o.niyiawosusi@gmail.come>
2022-01-07 19:20:45 +0000

    clear button now works as expected

--- src/js/components/githubIssues/search.tsx
+++ src/js/components/githubIssues/search.tsx
@@ -1,37 +1,36 @@
 import Button from '@salesforce/design-system-react/components/button';
 import InputIcon from '@salesforce/design-system-react/components/icon/input-icon';
-import Input from '@salesforce/design-system-react/components/input';
+import Search from '@salesforce/design-system-react/components/input/search';
 import { t } from 'i18next';
-import React from 'react';
-let searchterm = '';
-const Search = ({
+import React, { useState } from 'react';
+
+const SearchIssues = ({
   searchIssues,
 }: {
   searchIssues: (searcht: string) => void;
-}) => (
-  <>
-    <Input
-      onChange={(event, { value }) => {
-        event.preventDefault();
-        searchterm = value;
-      }}
-      placeholder={t('Search issues by title or number')}
-      name="search"
-      iconRight={
-        <InputIcon
-          assistiveText={{
-            icon: 'Clear',
-          }}
-          name="clear"
-          category="utility"
-          onClick={() => {
-            searchIssues('');
-          }}
-        />
-      }
-    ></Input>
-    <Button onClick={() => searchIssues(searchterm)}>{t('Search')}</Button>
-  </>
-);
+}) => {
+  const [searchterm, setSearchterm] = useState('');
+  const handleSearchterm = (event) => {
+    setSearchterm(event.target.value);
+  };
+  return (
+    <>
+      <Search
+        id="searchIssues"
+        placeholder={t('Search issues by title or number')}
+        name="search"
+        onChange={handleSearchterm}
+        value={searchterm}
+        clearable={true}
+        onClear={() => {
+          searchIssues('');
+          setSearchterm('');
+        }}
+        onSearch={() => searchIssues(searchterm)}
+      ></Search>
+      <Button onClick={() => searchIssues(searchterm)}>{t('Search')}</Button>
+    </>
+  );
+};
 
-export default Search;
+export default SearchIssues;
