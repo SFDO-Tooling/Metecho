0ab0de989ae9367bb4b00652fe5c6cc8cc120f27
Jonny Gerig Meyer <jonny@oddbird.net>
2022-02-11 17:11:27 -0500

    add progress indicator

---
+++ src/js/components/projects/createProjectModal/form.tsx
@@ -0,0 +1,53 @@
+import Input from '@salesforce/design-system-react/components/input';
+import Textarea from '@salesforce/design-system-react/components/textarea';
+import { t } from 'i18next';
+import React from 'react';
+
+import { CreateProjectData } from '@/js/components/projects/createProjectModal';
+import { UseFormProps } from '@/js/components/utils';
+
+interface Props {
+  inputs: CreateProjectData;
+  errors: UseFormProps['errors'];
+  handleInputChange: UseFormProps['handleInputChange'];
+}
+
+const CreateProjectForm = ({ inputs, errors, handleInputChange }: Props) => (
+  <form className="slds-form slds-p-around_large">
+    <Input
+      id="create-project-name"
+      label={t('Project Name')}
+      className="slds-form-element_stacked slds-p-left_none"
+      name="name"
+      value={inputs.name}
+      required
+      aria-required
+      errorText={errors.name}
+      onChange={handleInputChange}
+    />
+    <Input
+      id="create-project-repo_name"
+      label={t('GitHub Repository Name')}
+      className="slds-form-element_stacked slds-p-left_none"
+      name="repo_name"
+      value={inputs.repo_name}
+      required
+      aria-required
+      maxLength="100"
+      errorText={errors.repo_name}
+      onChange={handleInputChange}
+    />
+    <Textarea
+      id="create-project-description"
+      label={t('Description')}
+      classNameContainer="slds-form-element_stacked slds-p-left_none"
+      placeholder={t('Optional description of this Project')}
+      className="metecho-textarea"
+      name="description"
+      value={inputs.description}
+      errorText={errors.description}
+      onChange={handleInputChange}
+    />
+  </form>
+);
+export default CreateProjectForm;
--- src/js/components/projects/createProjectModal/index.tsx
+++ src/js/components/projects/createProjectModal/index.tsx
@@ -1,8 +1,10 @@
 import Button from '@salesforce/design-system-react/components/button';
 import Modal from '@salesforce/design-system-react/components/modal';
+import ProgressIndicator from '@salesforce/design-system-react/components/progress-indicator';
 import { t } from 'i18next';
 import React, { useState } from 'react';
 
+import CreateProjectForm from '@/js/components/projects/createProjectModal/form';
 import {
   LabelWithSpinner,
   useForm,
@@ -17,7 +19,7 @@ interface Props {
   closeModal: () => void;
 }
 
-interface CreateProjectData
+export interface CreateProjectData
   extends Pick<Project, 'name' | 'description' | 'repo_name' | 'github_users'> {
   organization: string;
   dependencies: string[];
@@ -95,6 +97,14 @@ const CreateProjectModal = ({ isOpen, closeModal }: Props) => {
     setInputs,
   });
 
+  // When project name changes, update repo_name
+  useFormDefaults({
+    field: 'repo_name',
+    value: inputs.name.replace(/[^A-Za-z0-9_.-]/g, '-').substring(0, 100),
+    inputs,
+    setInputs,
+  });
+
   const doSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
     setIsSaving(true);
     handleSubmit(e as any);
@@ -111,7 +121,6 @@ const CreateProjectModal = ({ isOpen, closeModal }: Props) => {
 
   const BackBtn = (args: any) => (
     <Button
-      key="back"
       label={t('Go Back')}
       variant="outline-brand"
       onClick={prevPage}
@@ -121,7 +130,6 @@ const CreateProjectModal = ({ isOpen, closeModal }: Props) => {
 
   const NextBtn = (args: any) => (
     <Button
-      key="submit"
       label={t('Save & Next')}
       variant="brand"
       onClick={nextPage}
@@ -129,53 +137,85 @@ const CreateProjectModal = ({ isOpen, closeModal }: Props) => {
     />
   );
 
+  const steps = [
+    { id: 0, label: t('Enter Project Details') },
+    { id: 1, label: t('Add Project Collaborators') },
+    { id: 2, label: t('Add Dependencies') },
+    { id: 3, label: t('Create Project') },
+  ];
+
+  const Progress = (
+    <ProgressIndicator
+      steps={steps}
+      completedSteps={steps.slice(0, pageIndex)}
+      selectedStep={steps[pageIndex]}
+      variant="modal"
+    />
+  );
+
   const pages = [
     {
       heading: t('Create Project'),
-      contents: null,
-      footer: [
-        <CancelBtn key="cancel" />,
-        <NextBtn key="next" disabled={!canSubmit} />,
-      ],
+      contents: (
+        <CreateProjectForm
+          inputs={inputs as CreateProjectData}
+          errors={errors}
+          handleInputChange={handleInputChange}
+        />
+      ),
+      footer: (
+        <div className="slds-grid slds-grid_align-spread">
+          <CancelBtn />
+          {Progress}
+          <NextBtn disabled={!canSubmit} />
+        </div>
+      ),
     },
     {
       heading: t('Add Project Collaborators'),
       contents: null,
-      footer: [
-        <BackBtn key="back" />,
-        <CancelBtn key="cancel" />,
-        <NextBtn key="next" />,
-      ],
+      footer: (
+        <div className="slds-grid slds-grid_align-spread">
+          <BackBtn />
+          {Progress}
+          <NextBtn />
+        </div>
+      ),
     },
     {
       heading: t('Add Dependencies'),
       contents: null,
-      footer: [
-        <BackBtn key="back" />,
-        <CancelBtn key="cancel" />,
-        <NextBtn key="next" />,
-      ],
+      footer: (
+        <div className="slds-grid slds-grid_align-spread">
+          <BackBtn />
+          {Progress}
+          <NextBtn />
+        </div>
+      ),
     },
     {
       heading: t('Create Project'),
       contents: null,
-      footer: [
-        <BackBtn key="back" />,
-        <CancelBtn key="cancel" />,
-        <Button
-          key="submit"
-          label={
-            isSaving ? (
-              <LabelWithSpinner label={t('Creating…')} variant="inverse" />
-            ) : (
-              t('Create Project')
-            )
-          }
-          variant="brand"
-          onClick={doSubmit}
-          disabled={isSaving}
-        />,
-      ],
+      footer: (
+        <div className="slds-grid slds-grid_align-spread">
+          <BackBtn />
+          {Progress}
+          <Button
+            key="submit"
+            label={
+              isSaving ? (
+                <LabelWithSpinner label={t('Creating…')} variant="inverse" />
+              ) : (
+                t('Create Project')
+              )
+            }
+            variant="brand"
+            onClick={doSubmit}
+            disabled={isSaving}
+          />
+          ,
+        </div>
+      ),
     },
   ];
 
@@ -187,7 +227,6 @@ const CreateProjectModal = ({ isOpen, closeModal }: Props) => {
       heading={pages[pageIndex].heading}
       assistiveText={{ closeButton: t('Cancel') }}
       footer={pages[pageIndex].footer}
-      directional={pageIndex > 0}
       onRequestClose={handleClose}
     >
       {pages[pageIndex].contents}
