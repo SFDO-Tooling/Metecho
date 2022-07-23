58feff581b48e8319f1e16525d91113cce18a01b
Stacy Kvernmo <stacy@oddbird.net>
2022-07-22 23:16:47 -0500

    edit layout of form panels in dataset capture modal

--- src/js/components/tasks/retrieveDataset/data.tsx
+++ src/js/components/tasks/retrieveDataset/data.tsx
@@ -205,7 +205,7 @@ export const SchemaList = ({
                 <strong>{t('Records')}</strong>
               </span>
             </div>
-            <div className="this-could-scroll">
+            <div className="metecho-scroll-panel">
               {sortedSchema.map(([groupName, groupSchema], index) => {
                 const uniqueGroupName = `${type}-${groupName}`;
                 const fields = map(groupSchema.fields, (pair) => pair[0]);
@@ -355,86 +355,88 @@ const RemovingList = ({
   };
 
   return (
-    <div
-      className={classNames(
-        'slds-form',
-        'slds-size_1-of-1',
-        'slds-p-top_large',
-        'slds-p-horizontal_large',
-        'has-checkboxes',
-        className,
-      )}
-      data-form="task-retrieve-changes"
-      // @@@
-      style={{ height: 'unset' }}
-      {...props}
-    >
-      <ModalCard
-        heading={
-          <span className="slds-m-left_xx-small">
-            {t('Existing Data To Remove')}
-          </span>
-        }
-        noBodyPadding
+    <div className="metecho-modal-content-wrapper">
+      <div
+        className={classNames(
+          'slds-form',
+          'slds-size_1-of-1',
+          'slds-p-top_large',
+          'slds-p-horizontal_large',
+          'has-checkboxes',
+          className,
+        )}
+        data-form="task-retrieve-changes"
+        // @@@
+        style={{ height: 'unset' }}
+        {...props}
       >
-        <p
-          className="slds-text-color_error
+        <ModalCard
+          heading={
+            <span className="slds-m-left_xx-small">
+              {t('Existing Data To Remove')}
+            </span>
+          }
+          noBodyPadding
+        >
+          <p
+            className="slds-text-color_error
             slds-m-left_xx-small
             slds-p-horizontal_medium
             slds-p-vertical_x-small"
-        >
-          <Trans i18nKey="outdatedSchemaWarning">
-            The dataset you selected contains fields that no longer exist in
-            this Dev Org. If you continue, the following data will be removed
-            from this dataset.
-          </Trans>
-        </p>
-        {chain(Object.keys(changes))
-          .sortBy(toLower)
-          .map((groupName, index) => {
-            const uniqueGroupName = `${type}-${groupName}`;
-            const fields = sortBy(changes[groupName], toLower);
+          >
+            <Trans i18nKey="outdatedSchemaWarning">
+              The dataset you selected contains fields that no longer exist in
+              this Dev Org. If you continue, the following data will be removed
+              from this dataset.
+            </Trans>
+          </p>
+          {chain(Object.keys(changes))
+            .sortBy(toLower)
+            .map((groupName, index) => {
+              const uniqueGroupName = `${type}-${groupName}`;
+              const fields = sortBy(changes[groupName], toLower);
 
-            return (
-              <Accordion key={uniqueGroupName} className="light-bordered-row">
-                <AccordionPanel
-                  expanded={Boolean(expandedPanels[uniqueGroupName])}
-                  key={`${uniqueGroupName}-panel`}
-                  id={`${type}-group-${index}`}
-                  onTogglePanel={() => handlePanelToggle(uniqueGroupName)}
-                  title={groupName}
-                  panelContentActions={
-                    <div className="form-grid">
-                      <span
-                        className="slds-text-body_regular
+              return (
+                <Accordion key={uniqueGroupName} className="light-bordered-row">
+                  <AccordionPanel
+                    expanded={Boolean(expandedPanels[uniqueGroupName])}
+                    key={`${uniqueGroupName}-panel`}
+                    id={`${type}-group-${index}`}
+                    onTogglePanel={() => handlePanelToggle(uniqueGroupName)}
+                    title={groupName}
+                    panelContentActions={
+                      <div className="form-grid">
+                        <span
+                          className="slds-text-body_regular
                           slds-p-top_xxx-small"
+                        >
+                          {groupName}
+                        </span>
+                      </div>
+                    }
+                    summary=""
+                  >
+                    {fields.map((fieldName) => (
+                      <div
+                        key={`${uniqueGroupName}-${fieldName}`}
+                        className="form-grid"
                       >
-                        {groupName}
-                      </span>
-                    </div>
-                  }
-                  summary=""
-                >
-                  {fields.map((fieldName) => (
-                    <div
-                      key={`${uniqueGroupName}-${fieldName}`}
-                      className="form-grid"
-                    >
-                      <span
-                        className="slds-text-body_regular
+                        <span
+                          className="slds-text-body_regular
                           slds-p-top_xxx-small
                           metecho-nested-checkboxes"
-                      >
-                        {fieldName}
-                      </span>
-                    </div>
-                  ))}
-                </AccordionPanel>
-              </Accordion>
-            );
-          })
-          .value()}
-      </ModalCard>
+                        >
+                          {fieldName}
+                        </span>
+                      </div>
+                    ))}
+                  </AccordionPanel>
+                </Accordion>
+              );
+            })
+            .value()}
+        </ModalCard>
+      </div>
     </div>
   );
 };
@@ -485,36 +487,37 @@ const DataForm = ({
     updateChecked(thisChange, checked);
   };
 
-  const classes =
-    'slds-form slds-size_1-of-1 slds-large-size_1-of-2 slds-p-horizontal_small';
+  const classes = 'slds-form';
 
   return (
     <>
-      {!isEmpty(outdatedChangeset) && (
-        <RemovingList changes={outdatedChangeset} />
-      )}
-      <div className="slds-p-around_large slds-grid slds-grid_pull-padded-small">
-        <SchemaList
-          className={classes}
-          type="all"
-          heading={t('Data Options')}
-          schema={schema}
-          checkedChanges={inputs.dataset_definition}
-          errors={errors.dataset_definition}
-          handleSelectGroup={handleSelectGroup}
-          handleChange={handleChange}
-        />
-        <SchemaList
-          className={classes}
-          type="selected"
-          heading={t('Selected Data')}
-          schema={selectedSchema}
-          errors={
-            noChanges && !isEmpty(selectedSchema)
-              ? t('Selected data matches existing dataset.')
-              : undefined
-          }
-        />
+      <div className="metecho-modal-inner-content">
+        {!isEmpty(outdatedChangeset) && (
+          <RemovingList changes={outdatedChangeset} />
+        )}
+        <div className="slds-p-around_large metecho-form-panels">
+          <SchemaList
+            className={classes}
+            type="all"
+            heading={t('Data Options')}
+            schema={schema}
+            checkedChanges={inputs.dataset_definition}
+            errors={errors.dataset_definition}
+            handleSelectGroup={handleSelectGroup}
+            handleChange={handleChange}
+          />
+          <SchemaList
+            className={classNames('my-new-class', classes)}
+            type="selected"
+            heading={t('Selected Data')}
+            schema={selectedSchema}
+            errors={
+              noChanges && !isEmpty(selectedSchema)
+                ? t('Selected data matches existing dataset.')
+                : undefined
+            }
+          />
+        </div>
       </div>
     </>
   );
