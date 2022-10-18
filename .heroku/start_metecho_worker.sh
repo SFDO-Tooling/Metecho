08b9083d6d21e4d0fb3bb09285ea7d476429180e
Josh Kofsky <jkofsky@salesforce.com>
2022-10-18 17:18:09 -0400

    Initial commit for vlocity heroku build environment dependencies

---
+++ .heroku/start_metecho_worker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# make Chrome available to VBT
+# Copied from https://github.com/SFDO-Tooling/MetaDeploy/pull/3468
+mkdir -p /opt/google/chrome
+ln -s /app/.apt/usr/bin/google-chrome /opt/google/chrome/chrome
+python manage.py rqworker default
