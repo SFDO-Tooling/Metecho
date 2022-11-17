3020b43a7e3b2e61121bb04350cb372bd282e8f2
David Ray <dray@salesforce.com>
2022-11-17 01:11:57 +0000

    add toast focus to project list

--- src/js/components/projects/list.tsx
+++ src/js/components/projects/list.tsx
@@ -240,7 +240,10 @@ const ProjectList = (
         </div>
 
         <div className="slds-p-around_x-large">
-          <div className="slds-grid slds-wrap slds-grid_pull-padded-small">
+          <div
+            tabIndex={-1}
+            className="metecho-toast-focus slds-grid slds-wrap slds-grid_pull-padded-small"
+          >
             <div
               className="slds-p-horizontal_small
                 slds-m-bottom_medium
