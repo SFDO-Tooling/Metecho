b5c769da7d5512b9cd736bae73c9f4d52eefb35a
Bharath Chadarajupalli <vchadarajupalli@salesforce.com>
2023-12-05 14:38:58 -0800

    @W-14603959 - code refactoring to fix the test

--- metecho/oauth2/salesforce/tests/provider.py
+++ metecho/oauth2/salesforce/tests/provider.py
@@ -1,14 +1,27 @@
 from ..provider import CustomSalesforceProvider
+from allauth.socialaccount.models import SocialApp
+import pytest
 
+@pytest.fixture
+def dummy_app():
+    app = SocialApp.objects.create(
+        provider=CustomSalesforceProvider.id,
+            name=CustomSalesforceProvider.id,
+            client_id="app123id",
+            key=CustomSalesforceProvider.id,
+            secret="dummy",
+    )
+    return app
 
-def test_get_auth_params(rf):
+@pytest.mark.django_db
+def test_get_auth_params(rf, dummy_app):
     request = rf.get("/")
-    result = CustomSalesforceProvider(request).get_auth_params(request, None)
+    result = CustomSalesforceProvider(request, dummy_app).get_auth_params(request, None)
     assert "prompt" in result and result["prompt"] == "login"
 
-
-def test_extract_uid(rf):
+@pytest.mark.django_db
+def test_extract_uid(rf, dummy_app):
     request = rf.get("/")
-    provider = CustomSalesforceProvider(request)
+    provider = CustomSalesforceProvider(request, dummy_app)
     result = provider.extract_uid({"organization_id": "ORG", "user_id": "USER"})
     assert result == "ORG/USER"
