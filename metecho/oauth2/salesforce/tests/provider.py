544d644b1fa7c53e72283e6d3faf27fcd003bdf4
Bharath Chadarajupalli <vchadarajupalli@salesforce.com>
2023-12-05 18:43:09 -0800

    @W-14603959 - fixing indentation

--- metecho/oauth2/salesforce/tests/provider.py
+++ metecho/oauth2/salesforce/tests/provider.py
@@ -2,22 +2,24 @@ from ..provider import CustomSalesforceProvider
 from allauth.socialaccount.models import SocialApp
 import pytest
 
+
 @pytest.fixture
 def dummy_app():
     app = SocialApp.objects.create(
         provider=CustomSalesforceProvider.id,
-            name=CustomSalesforceProvider.id,
-            client_id="app123id",
-            key=CustomSalesforceProvider.id,
-            secret="dummy",
-    )
+        name=CustomSalesforceProvider.id,
+        client_id="app123id",
+        key=CustomSalesforceProvider.id,
+        secret="dummy",)
     return app
+
 
 @pytest.mark.django_db
 def test_get_auth_params(rf, dummy_app):
     request = rf.get("/")
     result = CustomSalesforceProvider(request, dummy_app).get_auth_params(request, None)
     assert "prompt" in result and result["prompt"] == "login"
+
 
 @pytest.mark.django_db
 def test_extract_uid(rf, dummy_app):
