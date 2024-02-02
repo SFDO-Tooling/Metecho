795abbaad1ed60b668becfc7ecf7c45980d61421
lakshmi2506 <l.ramireddy@salesforce.com>
2024-02-02 11:16:18 +0000

    migrations

--- metecho/api/migrations/0118_project_deleted_at.py
+++ metecho/api/migrations/0118_project_deleted_at.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0117_scratchorg_installed_packages'),
+        ("api", "0117_scratchorg_installed_packages"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='project',
-            name='deleted_at',
+            model_name="project",
+            name="deleted_at",
             field=models.DateTimeField(blank=True, null=True),
         ),
     ]
